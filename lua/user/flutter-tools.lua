4f9095ad09b11aa61e330c89ff69938b2b208d34
euler <20284507+euler007@users.noreply.github.com>
2022-07-20 12:18:53 +0530

    Added flutter tools config

--- lua/user/flutter-tools.lua
+++ lua/user/flutter-tools.lua
@@ -1,1 +1,56 @@
-require("flutter-tools").setup{}
+require("flutter-tools").setup {
+  decorations = {
+    statusline = {
+      -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+      -- this will show the current version of the flutter app from the pubspec.yaml file
+      app_version = false,
+      -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+      -- this will show the currently running device if an application was started with a specific
+      -- device
+      device = false,
+    }
+  },
+  -- flutter_path = "<full/path/if/needed>", -- <-- this takes priority over the lookup
+  flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
+  fvm = false, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+  widget_guides = {
+    enabled = true,
+  },
+  dev_log = {
+    enabled = true,
+    open_cmd = "tabedit", -- command to use to open the log buffer
+  },
+  dev_tools = {
+    autostart = false, -- autostart devtools server if not detected
+    auto_open_browser = false, -- Automatically opens devtools in the browser
+  },
+  outline = {
+    open_cmd = "50vnew", -- command to use to open the outline buffer
+    auto_open = false -- if true this will open the outline automatically when it is first populated
+  },
+  lsp = {
+    color = { -- show the derived colours for dart variables
+      enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+      background = false, -- highlight the background
+      foreground = false, -- highlight the foreground
+      virtual_text = true, -- show the highlight using virtual text
+      virtual_text_str = "■", -- the virtual text character to highlight
+    },
+    -- on_attach = my_custom_on_attach,
+    -- capabilities = my_custom_capabilities -- e.g. lsp_status capabilities
+    -- --- OR you can specify a function to deactivate or change or control how the config is created
+    -- capabilities = function(config)
+    --   config.specificThingIDontWant = false
+    --   return config
+    -- end,
+    -- see the link below for details on each option:
+    -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+    settings = {
+      showTodos = true,
+      completeFunctionCalls = true,
+      -- analysisExcludedFolders = {"<path-to-flutter-sdk-packages>"},
+      renameFilesWithClasses = "prompt", -- "always"
+      enableSnippets = true,
+    }
+  }
+}
